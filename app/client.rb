c7c02aa66621a9d3dc2a32560d89f879c1f0804a
Volodymyr Stashchenko <stashchenko@fidor.de>
2018-04-04 16:26:37 +0300

    disable slack notifier

--- app/client.rb
+++ app/client.rb
@@ -4,7 +4,7 @@ module StockAgent
       def execute(stock_symbol, start_date, end_date)
         handler = StockAgent::StockHandler.new(stock_symbol, start_date, end_date)
         handler.add_notifier(StockAgent::Stdout)
-        handler.add_notifier(StockAgent::Slack)
+        # handler.add_notifier(StockAgent::Slack)
         handler.adapter = StockAgent::QuandlAdapter::Prices.new
 
         handler.show_period
